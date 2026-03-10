3c974c5ee58414f9984ff650891cbe9ad8d92f81
ahmedsharaf19 <engsharaf192002@gmail.com>
2026-03-11 00:33:33 +0200

    Partial Templates

--- Django_Course/challenges/urls.py
+++ Django_Course/challenges/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     #path('march', views.march),
     
     # Order Is Differnce So must to be care
-    path("", views.index),
+    path("", views.index, name = 'index'),
     path("<int:month>", views.monthly_challenge_by_number),
     path('<str:month>', views.monthly_challenge, name = 'month-challenge'),
     
