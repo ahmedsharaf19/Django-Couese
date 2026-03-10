7136ece6092b3f5472ee8c77572cd925976ad428
ahmedsharaf19 <engsharaf192002@gmail.com>
2026-03-10 23:41:46 +0200

    If TAg

--- Django_Course/challenges/views.py
+++ Django_Course/challenges/views.py
@@ -16,8 +16,9 @@ monthly_challenges = {
     'september': "Learn Django for at least 20 minutes every day!",
     'october': "Eat no meat for the entire month!",
     'november': 'Walk for at least 20 minutes every day!',
-    'december': "Learn Django for at least 20 minutes every day!",
-}
+    #'december': "Learn Django for at least 20 minutes every day!",
+    'december': None
+}   
 
 def index(request):
     list_items = ""
