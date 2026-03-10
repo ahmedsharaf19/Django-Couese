50ce9721c7d1753e37f6a9c76fc3123a94be84fb
ahmedsharaf19 <engsharaf192002@gmail.com>
2026-03-10 17:33:30 +0200

    Dynamic Path Segments

--- Django_Course/challenges/urls.py
+++ Django_Course/challenges/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 # this urls for challenges application
 urlpatterns = [
-    path('january', views.january),
-    path('february', views.febraury)   
+    #path('january', views.january),
+    #path('february', views.febraury),
+    #path('march', views.march),
+    path('<month>', views.monthly_challenge)
 ]
--- Django_Course/challenges/views.py
+++ Django_Course/challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
@@ -9,3 +9,18 @@ def january(request):
 
 def febraury(request):
     return HttpResponse("Walk for at least 20 minutes every day!")
+
+def march(request):
+    return HttpResponse("Learn Django for at least 20 minutes every day!")
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = 'Eat no meat for the entire month!'
+    elif month == 'february':
+        challenge_text = 'Walk for at least 20 minutes every day!'
+    elif month == 'march':
+            challenge_text = 'Learn Django for at least 20 minutes every day!'
+    else :
+        return HttpResponseNotFound('This month is not supported')
+    return HttpResponse(challenge_text) 
