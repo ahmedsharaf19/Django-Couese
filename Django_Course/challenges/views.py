b370414c8b7612de476a8f2c6f9767435d8143e1
ahmedsharaf19 <engsharaf192002@gmail.com>
2026-03-10 23:10:32 +0200

    For Tags

--- Django_Course/challenges/views.py
+++ Django_Course/challenges/views.py
@@ -21,12 +21,10 @@ monthly_challenges = {
 
 def index(request):
     list_items = ""
-    for month in monthly_challenges.keys():
-        path_url = reverse('month-challenge', args=[month])
-        list_items += f"<li><a href = \'{path_url}\'>{month.capitalize()}</a></li>"
-    
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    months = list(monthly_challenges.keys())
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
 
 def monthly_challenge_by_number(request, month):
